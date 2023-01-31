197f31b3292fa884d949e391025ab5c2edf9b853
manuel.zapien <manuel.zapien@MXLAGIL8107>
2023-01-31 14:49:01 -0600

    First commit

---
+++ accounting/src/main/java/com/agilethought/springboot/AcountApp.java
@@ -0,0 +1,12 @@
+package com.agilethought.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcountApp {
+	public static void main(String[] args) {
+		SpringApplication.run(AcountApp.class, args);
+	}
+
+}
