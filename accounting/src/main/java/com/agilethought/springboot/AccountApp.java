e169637d14bffc1bb9b8545ef5852a531b053729
manuel.zapien <manuel.zapien@host.docker.internal>
2023-02-08 17:47:24 -0600

    Hgo_Adjust for AWS Lamnda_MASZ

---
+++ accounting/src/main/java/com/agilethought/springboot/AccountApp.java
@@ -0,0 +1,20 @@
+package com.agilethought.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main Class for start Spring Boot Application
+ * @author Manuel Ashley Sanchez Zapien <mailto: manuel.zapien>
+ *
+ */
+@SpringBootApplication
+public class AccountApp {
+	/**
+	 * Staring Spring Boot Application method
+	 * @param args need arguments to pass to Spring Starters
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApp.class, args);
+	}
+
+}
