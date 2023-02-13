862f1dc019adf52dffde851dee5ce1cdb0c63b59
manuel.zapien <manuel.zapien@host.docker.internal>
2023-02-13 10:22:49 -0600

    Hgo_Creation of batch project_MASZ

---
+++ scheduler-pml/src/main/java/com/agilethought/schedulerpml/SchedulerPmlApplication.java
@@ -0,0 +1,13 @@
+package com.agilethought.schedulerpml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerPmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerPmlApplication.class, args);
+	}
+
+}
---
+++ scheduler-pml/src/test/java/com/agilethought/schedulerpml/SchedulerPmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agilethought.schedulerpml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerPmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
